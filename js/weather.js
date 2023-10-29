d7870b9ba77af54ac28e9060ad0e6e3af9d884a0
mc506lw <3462689887@qq.com>
2023-10-29 15:17:53 +0800

    Site updated: 2023-10-29 15:17:53

---
+++ js/weather.js
@@ -0,0 +1,26 @@
+<div id="he-plugin-simple"></div>
+<script>
+WIDGET = {
+  "CONFIG": {
+    "modules": "012",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "15",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "left",
+    "key": "574678e7424b47a089b94e5979a4ff5a"
+  }
+}
+</script>
+<script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script>
